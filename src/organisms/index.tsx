f34d68bf15092190965c4233c9b17593569fbd99
serzh <serzhan181@outlook.com>
2022-10-11 19:53:58 +0600

    refactor: fix export path

--- src/organisms/index.tsx
+++ src/organisms/index.tsx
@@ -1,4 +1,4 @@
 export { Header } from "./header";
 export { TrendingRow } from "./trending-row";
 export { PopularCommunities } from "./popular-communities";
-export { Accordion } from "./Accordion";
+export { Accordion } from "./accordion";
