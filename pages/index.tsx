4195066812aecadd390ec63fd59bd4f0431488a3
serzh <serzhan181@outlook.com>
2022-10-17 22:43:12 +0600

    refactor: ui code reconsidered

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,23 +6,28 @@ const Home: NextPage = () => {
   return (
     <>
       <div className="flex flex-wrap w-full flex-center">
-        <div className="flex flex-col gap-2">
-          <div className="flex mb-5">
-            <TrendingRow />
+        <div className="flex w-full mb-5">
+          <TrendingRow />
+        </div>
+
+        <div className="flex w-full">
+          <div className="flex flex-col gap-2 w-[70%]">
+            <ContentCard
+              imgSrc="https://images.unsplash.com/photo-1543357480-c60d40007a3f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZnJlZWRvbXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+              title="i like men"
+            />
+
+            <ContentCard
+              imgSrc="https://images.unsplash.com/photo-1665994658139-ba312e2bf060?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw2N3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+              title="KKK meeting. where and when"
+            />
           </div>
 
-          <div className="flex">
-            <div className="flex flex-col gap-2 w-[70%]">
-              <ContentCard />
-              <ContentCard />
-            </div>
+          <div className="flex gap-2 flex-col w-[30%] pl-2">
+            <PopularCommunities />
 
-            <div className="flex gap-2 flex-col w-[30%] pl-2">
-              <PopularCommunities />
-
-              {/* categories */}
-              <Categories />
-            </div>
+            {/* categories */}
+            <Categories />
           </div>
         </div>
       </div>
