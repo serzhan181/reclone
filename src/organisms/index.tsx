a4a91a7a71d711cd3ef0ec5ff12242aa1989fda7
serzh <serzhan181@outlook.com>
2022-10-23 14:57:04 +0600

    refactoring/small ui changes

--- src/organisms/index.tsx
+++ src/organisms/index.tsx
@@ -3,3 +3,4 @@ export { TrendingRow } from "./trending-row";
 export { PopularCommunities } from "./popular-communities";
 export { Accordion } from "./accordion";
 export { Categories } from "./categories";
+export { ContentMainPage } from "./content-main-page";
--- src/organisms/trending-row.tsx
+++ src/organisms/trending-row.tsx
@@ -7,7 +7,7 @@ export const TrendingRow = () => {
       <h3 className="text-sm font-semibold">Trending news</h3>
 
       {/* Cards themselves */}
-      <div className="flex justify-between gap-2">
+      <div className="flex flex-row flex-wrap justify-between h-56 overflow-hidden">
         <TrendingCard
           title="Trending news"
           description="pardon me sir? how did u get that"
