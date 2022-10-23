c1f4c7d582647abea0f177a7e7451c271b274956
serzh <serzhan181@outlook.com>
2022-10-23 14:09:21 +0600

    feature: modal molecule added

---
+++ src/molecules/modal.tsx
@@ -0,0 +1,27 @@
+import { FC, ReactNode } from "react";
+
+interface IModal {
+  active: boolean;
+  setActive: (isActive: boolean) => void;
+  children: ReactNode;
+}
+
+export const Modal: FC<IModal> = ({ active, setActive, children }) => {
+  return (
+    <div
+      className={`fixed top-0 left-0 z-50 flex w-screen h-screen opacity-0 pointer-events-none duration-500  bg-black bg-opacity-50 flex-center ${
+        active ? "opacity-100 pointer-events-auto" : "opacity-0"
+      }`}
+      onClick={() => setActive(false)}
+    >
+      <div
+        className={`p-4 bg-white rounded duration-300 ${
+          active ? "scale-100" : "scale-50"
+        }`}
+        onClick={(e) => e.stopPropagation()}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
