b0766d1f2410474a6977c18a75b309c3c85cf5d9
serzh <serzhan181@outlook.com>
2022-10-23 15:56:40 +0600

    auth: added & configured next-auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { DefaultLayout } from "@/src/layouts";
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{ session: Session }>) {
   return (
-    <DefaultLayout>
-      <Component {...pageProps} />
-    </DefaultLayout>
+    <SessionProvider session={pageProps.session}>
+      <DefaultLayout>
+        <Component {...pageProps} />
+      </DefaultLayout>
+    </SessionProvider>
   );
 }
