e8297054a26953ce8a2d4f6082957a85715dd99b
serzh <serzhan181@outlook.com>
2023-02-27 21:00:01 +0600

    no-absolute style

--- src/components/sidebar-menu.tsx
+++ src/components/sidebar-menu.tsx
@@ -24,7 +24,7 @@ export const SidebarMenu = () => {
   const pathname = usePathname();
 
   return (
-    <nav className="absolute left-0 flex flex-col gap-5 w-[20%]">
+    <nav className="flex flex-col gap-5 basis-[20%]">
       <div className="flex gap-2">
         <h2 className="text-sm font-semibold uppercase pl-9">Menu</h2>
       </div>
