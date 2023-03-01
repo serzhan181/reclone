f6e473f928dda745306ed94a476121498f0ff59b
serzh <serzhan181@outlook.com>
2023-03-01 18:00:57 +0600

    fix: returntype was incorrect

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,26 +3,29 @@ import { request } from "@/graphql/client";
 import { IMe } from "@/types";
 import UserIcon from "@heroicons/react/24/outline/UserCircleIcon";
 import { AuthButtons } from "./auth-buttons";
+import { cookies } from "next/headers";
+import Image from "next/image";
 
-async function getMe(): Promise<{ data: { me: IMe } }> {
+async function getMe(): Promise<{ me: IMe }> {
   try {
-    const data = await request<{ data: { me: IMe } }>(AUTH_ME);
+    const cookieStore = cookies();
+    const token = cookieStore.get(
+      process.env.NEXT_PUBLIC_AUTHORIZATION_COOKIE_NAME as string
+    );
+    const data = await request<{ me: IMe }>(AUTH_ME, undefined, token?.value);
+
     return data;
   } catch (error) {
     return {
-      data: {
-        me: {
-          authenticated: false,
-        },
+      me: {
+        authenticated: false,
       },
     };
   }
 }
 
 export const Header = async () => {
-  const {
-    data: { me },
-  } = await getMe();
+  const { me } = await getMe();
   return (
     <header className="fixed top-0 flex items-center justify-between w-full h-20 px-5 rounded-b shadow-md bg-primary">
       {/* Logo */}
@@ -38,7 +41,15 @@ export const Header = async () => {
         {me.authenticated ? (
           <button className="avatar">
             <div className="relative rounded-full w-14">
-              <UserIcon />
+              {me.user?.profile_picture_urn ? (
+                <Image
+                  src={me.user.profile_picture_urn}
+                  alt={me.user.username}
+                  fill
+                />
+              ) : (
+                <UserIcon />
+              )}
             </div>
           </button>
         ) : (
