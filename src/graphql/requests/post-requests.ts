c92a18171e486605c373c172ab9aaabb3c96a7e4
serzh <serzhan181@outlook.com>
2023-03-04 12:21:09 +0600

    more request functions

--- src/graphql/requests/post-requests.ts
+++ src/graphql/requests/post-requests.ts
@@ -1,9 +1,51 @@
-import { GET_POSTS } from "./../api/post-graphql";
+import { VOTE_ON_POST } from "./../api/vote-graphql";
+import { GetPostDetailed } from "./../../types/index";
+import { GET_POSTS, GET_POST_DETAILED } from "./../api/post-graphql";
 import { GetPost } from "@/types";
 import { request } from "../client";
 
-const getPosts = () => request<{ posts: GetPost[] }>(GET_POSTS);
+interface VoteResponse {
+  userVote: number;
+  voteScore: number;
+}
+
+interface VoteOnPost {
+  value: -1 | 0 | 1;
+  postId: string;
+}
+
+export interface WithToken {
+  token?: string;
+}
+
+interface GetPostVars {
+  identifier: string;
+  slug: string;
+}
+
+const getPosts = ({ token }: WithToken) =>
+  request<{ posts: GetPost[] }>(GET_POSTS, undefined, token);
+
+const getPost = ({ token, identifier, slug }: WithToken & GetPostVars) =>
+  request<{ post: GetPostDetailed }, GetPostVars>(
+    GET_POST_DETAILED,
+    {
+      identifier,
+      slug,
+    },
+    token
+  );
+
+const voteOnPost = ({ token, postId, value }: WithToken & VoteOnPost) => {
+  return request<{ vote: VoteResponse }, VoteOnPost>(
+    VOTE_ON_POST,
+    { postId, value },
+    token
+  );
+};
 
 export const postsRequests = {
   getPosts,
+  getPost,
+  voteOnPost,
 };
