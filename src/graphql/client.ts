daab6e44da2b89eabd1830e35983b471f346e1c7
serzh <serzhan181@outlook.com>
2023-03-04 12:19:26 +0600

    typesafe variables when request

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -4,18 +4,20 @@ export const gqlClient = new GraphQLClient(
   process.env.NEXT_PUBLIC_ENDPOINT_URL as string
 );
 
-export async function request<T = any>(
+export async function request<T = any, V = any>(
   query: string,
-  variables?: {} | undefined,
+  variables?: V,
   token?: string
 ): Promise<T> {
   if (token) {
     gqlClient.setHeader("Authorization", `Bearer ${token}`);
   }
 
-  return gqlClient.request<T>(query, variables).catch((err) => {
-    console.error("something went wrong", err);
+  return gqlClient
+    .request<T>(query, variables ? variables : {})
+    .catch((err) => {
+      console.error("something went wrong", err);
 
-    throw err.response.errors[0].message;
-  });
+      throw err.response.errors[0].message;
+    });
 }
