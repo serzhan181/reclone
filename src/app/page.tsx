88483fe60f832a9a5aa5688aa2a2aad4b8139696
serzh <serzhan181@outlook.com>
2023-03-04 12:23:10 +0600

    user vote props passed to posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import { Post } from "@/components/post";
 import { postsRequests } from "@/graphql/requests/post-requests";
+import { getToken } from "@/utils/get-token";
+
+export const dynamic = "force-dynamic";
 
 export default async function Home() {
-  const { posts } = await postsRequests.getPosts();
+  const token = getToken();
+  const { posts } = await postsRequests.getPosts({ token });
 
   return (
     <div className="flex flex-col gap-8">
@@ -12,11 +16,13 @@ export default async function Home() {
           href={`/r/${p.subName}/${p.identifier}/${p.slug}`}
           title={p.title}
           voteScore={p.voteScore}
+          userVote={p.userVote}
           subName={p.subName}
           authorName={p.user.username}
           createdAt={p.createdAt}
           commentCount={p.commentCount}
           subImg={p.subImgUrl}
+          id={p.id}
         />
       ))}
     </div>
