a71ee7561f6bc51b1b16f0c999ecdce5d455d960
serzh <serzhan181@outlook.com>
2023-03-25 16:56:05 +0600

    ui: dropdown shared component

---
+++ src/shared/ui/dropdown.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { Menu, Transition } from "@headlessui/react";
+import { Fragment } from "react";
+import EllipsisIcon from "@heroicons/react/20/solid/EllipsisVerticalIcon";
+import type { Option } from "@/types";
+
+interface DropdownProps {
+  options: Option[];
+  onClick: (value: Option["value"]) => void;
+}
+
+export const Dropdown = ({ options, onClick }: DropdownProps) => {
+  return (
+    <div className="text-right top-16">
+      <Menu as="div" className="relative inline-block text-left">
+        <div>
+          <Menu.Button className="btn btn-ghost btn-circle btn-xs">
+            <EllipsisIcon className="w-3" />
+          </Menu.Button>
+        </div>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0 w-56 mt-2 origin-top-right divide-y rounded-md shadow-lg bg-primary divide-base-100 ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="px-1 py-1 ">
+              {options.map((o) => (
+                <Menu.Item key={o.id}>
+                  {({ active }) => (
+                    <button
+                      className={`${
+                        active ? "bg-error" : "text-primary-content"
+                      } group text-white transition-all flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                      onClick={onClick.bind(null, o.value)}
+                    >
+                      {o.label}
+                    </button>
+                  )}
+                </Menu.Item>
+              ))}
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    </div>
+  );
+};
